e29720fd85498c29ac4722e0c3f4d66d42bdea72
Marek Meyer <marek_meyer@outlook.com>
2024-10-27 18:09:50 +0100

    Update to svelte 5

---
+++ src/lib/CategoryInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let saveExpense;
+</script>
+
+<div class="flex flex-col gap-4">
+	<div class="flex gap-4">
+		{#each availableCategories as availableCategory}
+			<button
+				class="btn text-xs font-light w-16"
+				class:btn-accent={category === availableCategory}
+				on:click={() => {
+					saveExpense(availableCategory);
+				}}>{availableCategory}</button
+			>
+		{/each}
+	</div>
+</div>
